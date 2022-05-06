a5590f8cb34cc6156542ffe152888049fb79693f
Oladele Tobiloba <tobiemma200@gmail.com>
2022-05-06 14:14:53 +0100

    update:create the build folder needed for production

--- frontend/src/pages/Sign In/sign-in.component.jsx
+++ frontend/src/pages/Sign In/sign-in.component.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./sign-in.styles.css";
+import FormInput from "../../components/form-input/form-input.component";
 
 const SignIn = () => {
-  return <div className="sign-in"></div>;
+  return (
+    <div className="sign-in">
+      <h2>Login</h2>
+      <form>
+        <FormInput type={"text"} name={"username"} placeholder={"username"} />
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
